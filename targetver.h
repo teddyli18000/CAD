7f3b5eae1f8101544c3a46cd9bf1749d3c9266d5
3803246429-alt <3803246429@qq.com>
2026-03-10 18:09:31 +0800

    Delete targetver.h

--- targetver.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// °üÀ¨ SDKDDKVer.h ½«¶¨Òå¿ÉÓÃµÄ×î¸ß°æ±¾µÄ Windows Æ½Ì¨¡£
-
-//Èç¹ûÒªÎªÒÔÇ°µÄ Windows Æ½Ì¨Éú³ÉÓ¦ÓÃ³ÌÐò£¬Çë°üÀ¨ WinSDKVer.h£¬²¢
-// ½« _WIN32_WINNT ºêÉèÖÃÎªÒªÖ§³ÖµÄÆ½Ì¨£¬È»ºóÔÙ°üÀ¨ SDKDDKVer.h¡£
-
-#include <SDKDDKVer.h>
